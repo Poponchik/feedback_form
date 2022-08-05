d0985177f0ff47c9cd84727d9f5251642fbb153e
Liza <liza13564281@gmail.com>
2022-08-05 12:54:43 +0300

    added .env and readme

---
+++ README.md
@@ -0,0 +1,15 @@
+Run application by following steps:
+
+1. Create new postgreSQL database called test_task
+
+2. Install client dependencies and run frontend
+ 2.1. cd client
+ 2.2 npm i
+ 2.3 npm start
+
+3. Copy .env.dist to .env, install server dependencies and run backend
+ 3.1 cd server
+ 3.2. cp .env.dist .env
+ 3.3 npm i
+ 3.4 npm run dev
+
--- server/db.js
+++ server/db.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize'
+import 'dotenv/config'
+console.log(process.env.DB_NAME)
 
-const sequelize = new Sequelize('postgres://postgres:1111@localhost:5432/test_task');
+const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/${process.env.DB_NAME}`);
 export default sequelize
